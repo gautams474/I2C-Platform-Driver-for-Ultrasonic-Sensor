f40d1a8dcd0f10b0686e4585504e3757d6c5c510
gautam <gsadaran@asu.edu>
2016-10-15 23:36:57 -0700

    removed hidden files

--- .HCSR_driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gautam/Projects/2/HCSR_driver.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/gautam/Projects/2/HCSR_driver.ko /home/gautam/Projects/2/HCSR_driver.o /home/gautam/Projects/2/HCSR_driver.mod.o
--- .tmp_versions/HCSR_driver.mod
+++
@@ -1,2 +0,0 @@
-/home/gautam/Projects/2/HCSR_driver.ko
-/home/gautam/Projects/2/HCSR_driver.o
