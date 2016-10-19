a86660cec56a9c937e30bc261e938421712a55c2
Prameet <prameetsk@yahoo.co.in>
2016-10-19 06:58:36 -0700

    Part 1 Kernel Space Tested.

--- cross-compiled/HCSR_driver.c
+++ cross-compiled/HCSR_driver.c
@@ -20,15 +20,15 @@
 #include <asm/msr.h>
 #include "common_data.h"
 
+#define DEBUG 1
+
 #define DEVICE_1	"HCSR_1"
 #define DEVICE_2	"HCSR_2"
 
-#define GPIO_ECHO 13
 #define GPIO_ECHO_LVL_SHFTR 34
 #define GPIO_ECHO_PULL_UP 35
 #define GPIO_ECHO_FMUX 77
 
-#define GPIO_TRIGGER 14
 #define GPIO_TRIGGER_LVL_SHFTR 16
 #define GPIO_TRIGGER_PULL_UP 17
 #define GPIO_TRIGGER_FMUX 76
@@ -67,22 +67,21 @@ static struct hcsr_dev{
 	struct task_struct* trigger_task_struct; // kernel thread used to trigger in coninuous mode and check state
 }*hcsr_devp[2];
 
-
 void gpio_init(int pin, char* name, int direction, int value){
 	int ret;
 	ret = gpio_request(pin, name);
 	if(ret)
-		printk("GPIO pin number %d could not be requested.\n", pin);
+		printk("%s: GPIO pin number %d could not be requested.\n",__FUNCTION__, pin);
 
 	if(direction == GPIO_INPUT){
 		ret = gpio_direction_input(pin);
 		if(ret)
-			printk("GPIO pin number %d could not be set as input.\n", pin);
+			printk("%s: GPIO pin number %d could not be set as input.\n",__FUNCTION__, pin);
 	}
 	else if(direction == GPIO_OUTPUT){
 		ret = gpio_direction_output(pin, value);
 		if(ret)
-			printk("GPIO pin number %d could not be set as output.\n", pin);
+			printk("%s: GPIO pin number %d could not be set as output.\n",__FUNCTION__, pin);
 		
 		// Direction output didn't seem to init correctly.		
 		gpio_set_value_cansleep(pin, value); 
@@ -95,22 +94,41 @@ void free_GPIOs(struct hcsr_dev* hcsr_devp){
 	int trigger = hcsr_devp->dev_gpio_pair.trigger;
 	unsigned int echo_irq =0;
 
-	if(hcsr_devp->trigger_task_struct != NULL)
+	if(hcsr_devp->trigger_task_struct != NULL && IS_MODE_CONTINUOUS(hcsr_devp)){
 		kthread_stop(hcsr_devp->trigger_task_struct);
+		hcsr_devp->trigger_task_struct = NULL;
+	}
 
 	if(echo != -1){
 		echo_irq = gpio_to_irq(echo);
 		free_irq(echo_irq, (void *)hcsr_devp);
 
 		gpio_free(echo);
+		hcsr_devp->dev_gpio_pair.echo = -1;
 	}
-	if(trigger != -1)
+	if(trigger != -1){
 		gpio_free(trigger);
+		hcsr_devp->dev_gpio_pair.trigger = -1;
+	}
+}
+
+
+void system_abort(void){
+	int i=0;
+	struct hcsr_dev* devp;
+	for(i=0; i<2; i++){
+		devp = hcsr_devp[i];
+
+		up(&(devp->buffer_signal));
+		free_GPIOs(devp);
+
+	}
 }
 
 /// IRQ Handler
+static unsigned long time_rise, time_fall, time_diff;
 static irq_handler_t echo_handler(int irq, void *dev_id, struct pt_regs *regs){
-	static unsigned long time_rise, time_fall, time_diff;
+	
 	static char toggle_entry =0;
 
 	struct hcsr_dev *hcsr_devp = (struct hcsr_dev *) dev_id;
@@ -123,12 +141,11 @@ static irq_handler_t echo_handler(int irq, void *dev_id, struct pt_regs *regs){
 		rdtscl(time_fall);
 		time_diff = time_fall - time_rise;
 		
-
 		hcsr_devp->buffer[hcsr_devp->head] = time_diff/(58*400);
-		hcsr_devp->head = (hcsr_devp->head+1)%5;
+		hcsr_devp->head = ((hcsr_devp->head) + 1) % 5;
 		if(hcsr_devp->head == hcsr_devp->tail)		// to ensure FIFO behavior
 			hcsr_devp->tail = (hcsr_devp->tail+1)%5;
-		hcsr_devp->size = (hcsr_devp->size +1) < 5 ? hcsr_devp->size + 1 : 5;
+		hcsr_devp->size = (hcsr_devp->size +1) < 6 ? hcsr_devp->size + 1 : 5;
 
 		irq_set_irq_type(irq,IRQF_TRIGGER_RISING);
 		up(&(hcsr_devp->buffer_signal));
@@ -142,7 +159,7 @@ static irq_handler_t echo_handler(int irq, void *dev_id, struct pt_regs *regs){
 static int hcsr_driver_open(struct inode *inode, struct file *file){
 	int device_no = 0;
 	struct miscdevice *c;
-	
+
 	device_no = MINOR(inode->i_rdev);
 	printk(KERN_ALERT"\nIn open, minor no = %d\n",device_no);
 	
@@ -154,6 +171,7 @@ static int hcsr_driver_open(struct inode *inode, struct file *file){
 			if(!strncmp(c->name, DEVICE_1, 6)){
 				printk(KERN_ALERT"HSCR 1 Opened\n");
 				file->private_data = hcsr_devp[0];
+
 				break;
 			}
 			else if(!strncmp(c->name, DEVICE_2, 6)){
@@ -163,33 +181,10 @@ static int hcsr_driver_open(struct inode *inode, struct file *file){
 			}
 			else{
 				printk(KERN_ERR"FILE OPEN: NAME NOT FOUND\n");
-				return -1;
+				return -EFAULT;
 			}
 		}
 	}
-
-
-	// /*TO DO remove this after user space is implemented*/
-	// gpio_init(GPIO_ECHO_FMUX, STRINGIFY(GPIO_ECHO_FMUX), 2, GPIO_LOW);
-	// gpio_init(GPIO_ECHO_PULL_UP, STRINGIFY(GPIO_ECHO_PULL_UP), GPIO_OUTPUT, GPIO_LOW);
-	// gpio_init(GPIO_ECHO_LVL_SHFTR, STRINGIFY(GPIO_ECHO_LVL_SHFTR), GPIO_OUTPUT, GPIO_HIGH);
-	// gpio_init(GPIO_ECHO, STRINGIFY(GPIO_ECHO), GPIO_INPUT, GPIO_LOW); // TO DO remove
-	
-	// gpio_init(GPIO_TRIGGER_FMUX, STRINGIFY(GPIO_TRIGGER_FMUX), 2, GPIO_LOW);
-	// gpio_init(GPIO_TRIGGER_PULL_UP, STRINGIFY(GPIO_TRIGGER_PULL_UP), GPIO_OUTPUT, GPIO_LOW);
-	// gpio_init(GPIO_TRIGGER_LVL_SHFTR, STRINGIFY(GPIO_TRIGGER_LVL_SHFTR), GPIO_OUTPUT, GPIO_LOW);
-	// gpio_init(GPIO_TRIGGER, STRINGIFY(GPIO_TRIGGER), GPIO_OUTPUT, GPIO_LOW); // TO DO remove
-
-
-	// echo_irq = gpio_to_irq(GPIO_ECHO);   // associate irq to echo pin
-
-	// ret = request_irq(echo_irq, (irq_handler_t)echo_handler, IRQF_TRIGGER_RISING, "Echo_Dev", hcsr_devp[0]);
-	// if(ret < 0){
-	// 	printk("Error requesting IRQ: %d\n", ret);
-	// }
-
-	
-	
 	return 0;
 }
 
@@ -215,16 +210,19 @@ int trigger_func(void* data){
 		while(!kthread_should_stop()){
 			trigger_HCSR(hcsr_devp);
 			msleep(time);
-			//printk("\t\ttriggered !!\n");
+			#if DEBUG
+				printk("\t\t%s: %s triggered !!\n",__FUNCTION__, hcsr_devp->misc_dev.name);
+			#endif
 		}
 	}
 	else if(mode == MODE_ONE_SHOT){
 		trigger_HCSR(hcsr_devp);
-		hcsr_devp->trigger_task_struct = NULL;
 		msleep(60);  						// minimum sleep required between two triggers as mentioned in datasheet		
 	}
 	else
-		printk(KERN_ERR"%s: %d Incorrect Mode", __FUNCTION__, hcsr_devp->dev_mode_pair.mode);
+		printk(KERN_ERR"%s: %d Incorrect Mode\n", __FUNCTION__, hcsr_devp->dev_mode_pair.mode);
+
+	hcsr_devp->trigger_task_struct = NULL;
 
 	return 0;
 }
@@ -244,28 +242,30 @@ static ssize_t hcsr_driver_write(struct file *file, const char *buf,size_t count
 	int input, ret = 0, i;
 	unsigned long time;
 	
-	if(buf == NULL)
-		printk("buf NULL\n");
+	if(buf == NULL){
+		printk("%s: buf NULL\n",__FUNCTION__);
+		return -EFAULT;
+	}
 	
 	if(copy_from_user(&input, buf, sizeof(int)) != 0)
 		return -EFAULT;
 
 	if(hcsr_devp->dev_mode_pair.mode == MODE_ONE_SHOT){  //one shot mode
-
-		if(input){  // clear buffer for non zero input
-			for(i = 0; i< 5; i++){
+		hcsr_devp->size = 0;
+		hcsr_devp->tail = 0;
+		hcsr_devp->head = 0;
+		if(input)  // clear buffer for non zero input
+			for(i = 0; i< 5; i++)
 				hcsr_devp->buffer[i] = -1;
-				hcsr_devp->size = 0;
-				hcsr_devp->tail = 0;
-				hcsr_devp->head = 0;
-			}
-		}
 
+		#if DEBUG
+			printk("%s: buffer cleared\n",__FUNCTION__);
+		#endif
+		
 		if(hcsr_devp->trigger_task_struct == NULL)  // if not triggered, start triggering
 			ret = start_triggers(hcsr_devp);
 		else
 			printk(KERN_ERR "%s: trigger_task_struct is not null\n",__FUNCTION__);
-
 	}
 	else if(hcsr_devp->dev_mode_pair.mode == MODE_CONTINUOUS){  //continous mode 
 		if(input == STOP_CONT_TRGGR){ // stop continuous triggering
@@ -289,19 +289,28 @@ static ssize_t hcsr_driver_read(struct file *file, char *buf, size_t count, loff
 	struct hcsr_dev *hcsr_devp = file->private_data;
 	long val;
 
-	if(BUFFER_EMPTY(hcsr_devp) && IS_STOPPED(hcsr_devp) && IS_MODE_ONE_SHOT(hcsr_devp))
+	if(BUFFER_EMPTY(hcsr_devp) && IS_STOPPED(hcsr_devp) && IS_MODE_ONE_SHOT(hcsr_devp)){
 			trigger_HCSR(hcsr_devp);
-
-		// to avoid against sleeping indefinately when continuous mode is not on
+			#if DEBUG
+				printk(KERN_ALERT "%s: triggering in one shot mode\n",__FUNCTION__);
+			#endif
+		}
+	// Avoids sleeping indefinately when continuous mode is not on
 	if(BUFFER_EMPTY(hcsr_devp) && IS_MODE_CONTINUOUS(hcsr_devp)){
-		printk(KERN_ERR "%s: Buffer empty and mode is continuous.", __FUNCTION__);
-		return -EFAULT;
+		#if DEBUG
+		printk(KERN_ERR "%s: Buffer empty and mode is continuous.\n", __FUNCTION__);
+		#endif
+		return -EINPROGRESS;
 	}
 
 	//sleep while buffer is empty
-	printk("before sleeping\n");
+	#if DEBUG
+		printk("size: %d before sleeping\n", hcsr_devp->size);
+	#endif
+
 	if(down_interruptible(&(hcsr_devp->buffer_signal))){
 		printk(KERN_ALERT "%s: semaphore interrupted\n",__FUNCTION__);
+		system_abort();
 		return -EFAULT;// semaphore interrupted
 	}
 
@@ -309,9 +318,12 @@ static ssize_t hcsr_driver_read(struct file *file, char *buf, size_t count, loff
 	hcsr_devp->tail = (hcsr_devp->tail+1)%5;
 	hcsr_devp->size = (hcsr_devp->size -1) > 0 ? hcsr_devp->size - 1 : 0;
 
-	printk(KERN_ALERT "val: %lu \n\n", val);
+	#if DEBUG
+	printk(KERN_ALERT "val: %ld time_diff: %ld\n\n", val, time_diff/(58l*400l));
+	#endif
+
 	
-	if(copy_to_user(buf, &val, sizeof(long) != 0))
+	if(copy_to_user(buf, &val, sizeof(long)) != 0)
 		return -EFAULT;
 
 	//returning bytes read
@@ -329,15 +341,11 @@ static long HCSR_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
 			
 			if(copy_from_user(&(hcsr_devp->dev_mode_pair), (struct mode_pair*)arg, sizeof(struct mode_pair)) != 0)
 				return -EFAULT;
-<<<<<<< HEAD
-			if(hcsr_devp->dev_mode_pair.mode != MODE_CONTINUOUS && hcsr_devp->dev_mode_pair.mode != MODE_ONE_SHOT){
-				printk("%s: wrong mode %d \n",__FUNCTION__, hcsr_devp->dev_mode_pair.mode);
-=======
 
-			printk("mode : %d , freequency : %d \n",hcsr_devp->dev_mode_pair.mode, hcsr_devp->dev_mode_pair.frequency);
+			// printk("mode : %d , frequency : %d \n",hcsr_devp->dev_mode_pair.mode, hcsr_devp->dev_mode_pair.frequency);
 			if(hcsr_devp->dev_mode_pair.mode != MODE_CONTINUOUS && hcsr_devp->dev_mode_pair.mode != MODE_ONE_SHOT){
 				printk("%s: wrong mode %d\n",__FUNCTION__, hcsr_devp->dev_mode_pair.mode);
->>>>>>> upstream/master
+				
 				return -EFAULT;
 			}
 
@@ -364,15 +372,18 @@ static long HCSR_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
 			if(hcsr_devp->dev_gpio_pair.echo < 0 || hcsr_devp->dev_gpio_pair.trigger < 0)
 				return -EFAULT;
 			
-			gpio_init(hcsr_devp->dev_gpio_pair.echo, STRINGIFY(GPIO_ECHO), GPIO_INPUT, GPIO_LOW);
+			gpio_init(hcsr_devp->dev_gpio_pair.echo, STRINGIFY(GPIO_ECHO), GPIO_INPUT, -1);
 			gpio_init(hcsr_devp->dev_gpio_pair.trigger, STRINGIFY(GPIO_TRIGGER), GPIO_OUTPUT, GPIO_LOW);
 
 			echo_irq = gpio_to_irq(hcsr_devp->dev_gpio_pair.echo);
 			ret = request_irq(echo_irq, (irq_handler_t)echo_handler, IRQF_TRIGGER_RISING, "Echo_Dev", hcsr_devp);
 			if(ret < 0){
 				printk("Error requesting IRQ: %d\n", ret);
-				return -1;
+				return -EFAULT;
 			}
+			
+			printk(KERN_ALERT"%s: echo: %d  echo_irq: %d trigger: %d   for device %s\n",
+								__FUNCTION__, hcsr_devp->dev_gpio_pair.echo, echo_irq, hcsr_devp->dev_gpio_pair.trigger, hcsr_devp->misc_dev.name);
 			return 0;
 		default:
 			return -EFAULT;
@@ -407,15 +418,15 @@ static struct miscdevice hcsr_dev2 = {
 static int __init hcsr_driver_init(void){
 	int ret;
 
-	hcsr_devp[0] = kmalloc(sizeof(struct hcsr_dev), GFP_KERNEL);
+	hcsr_devp[0] = kzalloc(sizeof(struct hcsr_dev), GFP_KERNEL);
 	if(!hcsr_devp[0]){
-			printk("Kmalloc failed 1\n");
+			printk("%s: Kmalloc 1 failed\n",__FUNCTION__);
 			return -1;
 	}
 
-	hcsr_devp[1] = kmalloc(sizeof(struct hcsr_dev), GFP_KERNEL);
+	hcsr_devp[1] = kzalloc(sizeof(struct hcsr_dev), GFP_KERNEL);
 	if(!hcsr_devp[1]){
-			printk("Kmalloc failed");
+			printk("%s: Kmalloc 2 failed\n",__FUNCTION__);
 			return -1;
 	}
 
@@ -466,19 +477,13 @@ static int __init hcsr_driver_init(void){
 /* Driver Exit */
 void __exit hcsr_driver_exit(void){
 
-	/*TO DO Check status before stopping both drivers*/
-	if(hcsr_devp[0]->trigger_task_struct != NULL)
-		kthread_stop(hcsr_devp[0]->trigger_task_struct);
+	system_abort();
 
-	if(hcsr_devp[1]->trigger_task_struct != NULL)
-		kthread_stop(hcsr_devp[1]->trigger_task_struct);
-
-	kfree(hcsr_devp[0]);
-	kfree(hcsr_devp[1]);
 	misc_deregister(&hcsr_dev1);
 	misc_deregister(&hcsr_dev2);
-	
-	//
+	kfree(hcsr_devp[0]);
+	kfree(hcsr_devp[1]);
+
 	printk("hcsr driver removed.\n");
 }
 
--- cross-compiled/sendScript.sh
+++ cross-compiled/sendScript.sh
@@ -1,3 +1,10 @@
+ #!/bin/bash
+echo
 make clean
+echo 
 make
-scp -r /home/gautam/Projects/2/cross-compiled/ root@192.168.1.5:/home/root
+echo
+echo Sending Files ......
+scp -r $PWD/*.ko $PWD/main $PWD/faltu_main $PWD/faltu_main2 $PWD/*.h root@192.168.1.5:/home/root
+echo 
+#scp -r $PWD/(main) root@192.168.1.5:/home/root
